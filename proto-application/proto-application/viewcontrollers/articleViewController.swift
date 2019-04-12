0dd0285741169cda1fa1cd07157d381e20bc1e2b
Hessel Bierma <h.bierma@me.com>
2019-04-12 10:56:26 +0200

    News articles no more editable back button in article screen Logging out fixed again

--- proto-application/proto-application/viewcontrollers/articleViewController.swift
+++ proto-application/proto-application/viewcontrollers/articleViewController.swift
@@ -42,14 +42,8 @@ class articleViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func backPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
     }
-    */
-
+    
 }
