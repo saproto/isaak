749c75138bca9cdef8ec666ea220cb311395dc2e
Hessel Bierma <h.bierma@me.com>
2019-04-13 22:52:28 +0200

    manually merged protube remote into this branch improved omnomcom checkout

---
+++ proto-application/proto-application/Table Cells/ytVideoCell.swift
@@ -0,0 +1,33 @@
+//
+//  ytVideoCell.swift
+//  proto-application
+//
+//  Created by Hessel Bierma on 03/04/2019.
+//  Copyright © 2019 S.A. Proto. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import AlamofireImage
+
+class ytVideoCell: UITableViewCell {
+
+    var thumbnailURL: String = ""
+    @IBOutlet var Thumbnail: UIImageView!
+    @IBOutlet var title: UILabel!
+    @IBOutlet var artist: UILabel!
+    @IBOutlet var duration: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
